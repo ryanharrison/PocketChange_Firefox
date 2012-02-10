0f113bc6660b29008b92ef7ba4106bc8316c10c7
Ryan Harrison <harrison.ryanm@gmail.com>
2012-02-10 00:21:31 -0800

    Added saveToFile function

--- content/PocketChange.js
+++ content/PocketChange.js
@@ -3,29 +3,36 @@ var EXPORTED_SYMBOLS = [ "PocketChange" ];
 //const Cc = Components.classes;
 //const Ci = Components.interfaces;	
 
+Components.utils.import("resource://gre/modules/NetUtil.jsm");  
+Components.utils.import("resource://gre/modules/FileUtils.jsm"); 
+
 
 if ("undefined" == typeof(PocketChange)) {
   var PocketChange = {};  
 };
 
-// PocketChange.Settings = {
-// 	_rhString : 'fubar string',
-
-// 	get rhString() { return this._rhString; },
-
-// 	changeString : function(newString) {
-// 		this._rhString = newString;
-// 	}
-// }
+PocketChange.Settings = {
+	
+	loadSettings : function() {
+		
+	},
+	saveSettings : function() {
+		var filename, data;
+		filename = "fubar.txt";
+		data = "lolz";
+		PocketChange.FileAccess.saveToFile(filename, data);
+	}
+}
 
 PocketChange.ButtonController = {
 	mainButtonClick : function() {
 		jQuery("#pocketchange-button").css("list-style-image","url('chrome://pocketchange/content/images/button_big_deselected.png'");
-		alert("main toolbar button clicked!");
+		//alert("main toolbar button clicked!");
+		PocketChange.Settings.saveSettings();
 	}
 }
 
-PocketChange.FormController = {		
+PocketChange.FormController = {
 	_formWidth : 300,	// Width in pixels
 	_formHeader : 'PocketChange',
 	_orderAmount : 57.77,
@@ -99,5 +106,43 @@ PocketChange.ProjectsController = {
 		} else {
 			this._selectedProject = project;
 		}
+	}
+}
+
+PocketChange.FileAccess = {
+	saveToFile : function(filename, data) {
+		var localDir, file, ostream, converter, istream;
+
+		// Get the directory that the file will be saved in.
+		// If the directory does not exist, create it.
+		localDir = new FileUtils.getDir("ProfD",["pocketchange"], true, false);
+
+		// Append the filename.
+		localDir.append(filename);
+		
+		// Create a file object to store settings.
+		file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);		
+
+		// Inititalize the settings file (create it).
+		file.initWithPath(localDir.path);
+
+		ostream = FileUtils.openSafeFileOutputStream(file);
+		converter = Components.classes["@mozilla.org/intl/scriptableunicodeconverter"].createInstance(Components.interfaces.nsIScriptableUnicodeConverter);  
+		converter.charset = "UTF-8";
+
+		istream = converter.convertToInputStream(data);
+		
+		//The last argument (the callback) is optional.		
+		NetUtil.asyncCopy(istream, ostream, function(status) {  
+			if (!Components.isSuccessCode(status)) {  
+				// Handle error!  
+				alert('found error');
+				dump("file error:\n"+status+"\n\n");
+				return;  
+			}
+			
+			// Data has been written to the file.
+		  	alert('Data has been written to the file');
+		});
 	}
 }
