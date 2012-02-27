af8546a629be07b8bb5d709ce1ccb008c23cfcf4
Ryan Harrison <harrison.ryanm@gmail.com>
2012-02-27 15:48:34 -0800

    Added preferences system

--- defaults/preferences/pocketchange.js
+++ defaults/preferences/pocketchange.js
@@ -1,7 +1,2 @@
 pref("extensions.pocketchange.donationRate", 2);
 pref("extensions.pocketchange.enabled", true);
-
-
-// pref("extensions.pocketchange.fubar", 7);
-// pref("extensions.pocketchange.taxes", true);
-// pref("extensions.pocketchange.subbranch.pref3", 0);
